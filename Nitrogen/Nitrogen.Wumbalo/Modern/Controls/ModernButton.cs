244d6ae8e9b74e519538070fe1f60ef81edcae39
Alex Reed (Xerax) <alexreed@outlook.com>
2013-11-28 23:43:37 +0000

    Custom modern button control added

---
+++ Nitrogen/Nitrogen.Wumbalo/Modern/Controls/ModernButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nitrogen.Wumbalo.Modern.Controls
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ModernButton : Button
+	{
+		public static DependencyProperty ButtonNameProperty;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		static ModernButton()
+		{
+			ButtonNameProperty = DependencyProperty.Register("ButtonName", typeof(String), typeof(ModernButton));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public String ButtonName
+		{
+			get { return (String)GetValue(ButtonNameProperty); }
+			set { SetValue(ButtonNameProperty, value); }
+		}
+	}
+}
