85e8679885254bf7a091a7aac38ca25c6c2e0a60
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-02 15:17:22 +0000

    New converter ^_^

---
+++ Nitrogen/Nitrogen.Wumbalo/Converters/EnumToString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Nitrogen.Wumbalo.ViewModels.Dialogs;
+
+namespace Nitrogen.Wumbalo.Converters
+{
+	[ValueConversion(typeof(ModernMessageBoxButton), typeof(string))]
+	public class EnumToString : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return ((ModernMessageBoxButton) value).ToString("G");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
